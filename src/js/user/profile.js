020eb02c27b69d7b0f63056c4a1137eb0a7714aa
Ben <510662506.@qq.com>
2017-09-10 21:50:07 +0800

    实现更新个人资料功能

--- src/js/user/profile.js
+++ src/js/user/profile.js
@@ -11,4 +11,16 @@ $.ajax({
             }
         }
     })
-    //
+    //页面的动态异步生成的，
+$("#user-profile-form").ajaxForm({
+    delegation: true,
+    success: function(data) {
+        if (data.code == 200) {
+            alert("保存成功")
+            window.location.Reload()
+        }
+    },
+    error: function() {
+        alert("保存失败")
+    }
+})
