98d815a1236676033f09525d4155c6bd5d1cfd56
Ben <510662506.@qq.com>
2017-09-10 21:14:26 +0800

    修改秘密页面，完善提交前密码校验功能

--- src/js/user/repass.js
+++ src/js/user/repass.js
@@ -1,12 +1,22 @@
 require("../common/header")
 require("../common/aside")
-$("#repass-form").ajaxForm({
-    success: function(data) {
-        if (data.code == 200) {
-            alert("保存成功")
-        } else(alert("保存失败"))
-    },
-    error: function() {
-        alert("保存失败")
+$("#repass-form").on("submit", function() {
+    //表单验证
+    if ($("#input-pass").val() !== $("#input-pass-reset").val()) {
+        alert("密码不一致")
+        return false;
     }
+    $("#repass-form").ajaxSubmit({
+        success: function(data) {
+            if (data.code == 200) {
+                alert("保存成功");
+                //重置表单
+                $('#repass-form')[0].reset()
+            }
+        },
+        error: function() {
+            alert("原密码错误")
+        }
+    })
+    return false;
 })
