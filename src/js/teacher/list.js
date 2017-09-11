e22cf0d95830861c6fc1b797011d767c0b335609
Ben <510662506.@qq.com>
2017-09-11 19:16:53 +0800

    讲师列表渲染

--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -1,2 +1,6 @@
 require("../common/header");
 require("../common/aside");
+//列表页面渲染
+$.get("/v6/teacher", function(data) {
+    $(".table-striped").append(template("teacher-list-tpl", data.result))
+})
