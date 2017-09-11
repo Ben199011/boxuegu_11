f0828d6112b98aa1950309426081d5bc2dc627ed
Ben <510662506.@qq.com>
2017-09-11 12:06:39 +0800

    实现学科列表回显

--- src/js/category/list.js
+++ src/js/category/list.js
@@ -1,2 +1,7 @@
 require("../common/header")
 require("../common/aside")
+$.get("/v6/category", function(data) {
+    if (data.code == 200) {
+        $(".table-bordered").append(template("category-list-tpl", data.result))
+    }
+})
