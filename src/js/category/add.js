4cac3446a3e9e3af2fda9cbc85deaee6c7bd0adb
Ben <510662506.@qq.com>
2017-09-11 12:37:38 +0800

    实现添加学科页面的功能

--- src/js/category/add.js
+++ src/js/category/add.js
@@ -1,2 +1,12 @@
 require("../common/header")
 require("../common/aside")
+    // 动态创建模板
+$.get("/v6/category/top", function(data) {
+        $("#select-add-tpl").html(template("category-top-tpl", data.result))
+    })
+    //添加学科
+$("#category-add-form").ajaxForm(function(data) {
+    if (data.code == 200) {
+        alert("保存成功")
+    }
+})
