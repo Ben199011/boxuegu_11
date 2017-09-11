bf693552a5465c7570c0e7007cd206ab804cc21e
Ben <510662506.@qq.com>
2017-09-11 21:21:13 +0800

    讲师编辑页面的渲染

--- src/js/teacher/edit.js
+++ src/js/teacher/edit.js
@@ -1,2 +1,10 @@
-require("../common/header")
-require("../common/aside")
+require("../common/header");
+require("../common/aside");
+var util = require("../common/util");
+var tc_id = util.getSearch("tc_id");
+//讲师编辑页面的 渲染
+$.get("/v6/teacher/edit", { "tc_id": tc_id }, function(data) {
+    if (data.code == 200) {
+        $(".teacher-edit").html(template("teacher-edit-form", data.result))
+    }
+})
