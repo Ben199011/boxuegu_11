59bc91eccdc74035850342e2c5a481531707d70d
Ben <510662506.@qq.com>
2017-09-11 20:20:48 +0800

    讲师列表中的查看的渲染

--- src/js/teacher/list.js
+++ src/js/teacher/list.js
@@ -6,14 +6,23 @@ $.get("/v6/teacher", function(data) {
     })
     //教师列表的启用和注销功能
 $(document).on("click", ".btn-teacher-list", function() {
-    var _this = $(this);
-    var data = {
-        "tc_id": $(this).attr("data-id"),
-        "tc_status": $(this).attr("data-status")
-    }
-    $.get("/v6/teacher/handle", data, function(data) {
-        var newStatus = data.result.tc_status;
-        _this.text(newStatus == 0 ? '注 销' : '启 用');
-        _this.attr("data-status", newStatus);
+        var _this = $(this);
+        var data = {
+            "tc_id": $(this).attr("data-id"),
+            "tc_status": $(this).attr("data-status")
+        }
+        $.get("/v6/teacher/handle", data, function(data) {
+            var newStatus = data.result.tc_status;
+            _this.text(newStatus == 0 ? '注 销' : '启 用');
+            _this.attr("data-status", newStatus);
+        })
+    })
+    //讲师列表的查看
+$(document).on("click", ".btn-teacher-watch", function() {
+    var tc_id = $(this).attr("data-id");
+    console.log(tc_id)
+    console.log(123)
+    $.get("/v6/teacher/view", { "tc_id": tc_id }, function(data) {
+        $("#teacherModal").html(template("teacherModal-watch-tpl", data.result))
     })
 })
