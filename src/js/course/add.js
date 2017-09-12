92a0c868d5805613572f1a629083cba191dd11dd
Ben <510662506.@qq.com>
2017-09-12 10:44:01 +0800

    添加课程完成

--- src/js/course/add.js
+++ src/js/course/add.js
@@ -1,2 +1,19 @@
-require("../common/header")
-require("../common/aside")
+require("../common/header");
+require("../common/aside");
+//用插件做要把a标签改为button标签，type改为submit
+// $("#course-add-form").ajaxForm(function(data) {
+//     if (data.code == 200) {
+//         alert("创建成功")
+//     }
+// })
+$(".btn-course-add").on("click", function() {
+    $.ajax({
+        url: "/v6/course/create",
+        data: $("#course-add-form").serialize(),
+        success: function(data) {
+            if (data.code == 200) {
+                alert("创建成功")
+            }
+        }
+    })
+})
