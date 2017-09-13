40d113b2a20a547b7e3e1411066bba29fb2e3dc9
Ben <510662506.@qq.com>
2017-09-13 10:19:55 +0800

    加id

--- src/js/course/add.js
+++ src/js/course/add.js
@@ -1,19 +1,20 @@
 require("../common/header");
 require("../common/aside");
 //用插件做要把a标签改为button标签，type改为submit
-// $("#course-add-form").ajaxForm(function(data) {
-//     if (data.code == 200) {
-//         alert("创建成功")
-//     }
-// })
-$(".btn-course-add").on("click", function() {
-    $.ajax({
-        url: "/v6/course/create",
-        data: $("#course-add-form").serialize(),
-        success: function(data) {
-            if (data.code == 200) {
-                alert("创建成功")
-            }
+$("#course-add-form").ajaxForm(function(data) {
+        if (data.code == 200) {
+            alert("创建成功");
+            location.href = '/dist/html/course/course_edit_step1.html?cs_id=' + data.result.cs_id;
         }
     })
-})
+    // $(".btn-course-add").on("click", function() {
+    //     $.ajax({
+    //         url: "/v6/course/create",
+    //         data: $("#course-add-form").serialize(),
+    //         success: function(data) {
+    //             if (data.code == 200) {
+    //                 alert("创建成功")
+    //             }
+    //         }
+    //     })
+    // })
