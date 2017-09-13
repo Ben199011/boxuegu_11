9aaddcd5656baa57b0624e24a387584938b1cd05
Ben <510662506.@qq.com>
2017-09-13 15:34:30 +0800

    课程编辑第二步数据回显

--- src/js/course/edit2.js
+++ src/js/course/edit2.js
@@ -1,2 +1,11 @@
 require("../common/header")
 require("../common/aside")
+var util = require("../common/util");
+var cs_id = util.getSearch('cs_id');
+//数据回显
+$.get("/v6/course/picture", { cs_id: cs_id }, function(data) {
+    if (data.code == 200) {
+        data.result.editIndex = 2;
+        $('#course-edit2').append(template('course-edit2-tpl', data.result))
+    }
+})
