a4113967858713c161bd9f07ab0ae07384c6b7cf
Ben <510662506.@qq.com>
2017-09-13 14:01:35 +0800

    课程编辑1数据回显

--- src/js/course/edit1.js
+++ src/js/course/edit1.js
@@ -1,2 +1,11 @@
 require("../common/header")
-require("../common/aside")
+require("../common/aside");
+var util = require('../common/util')
+var cs_id = util.getSearch("cs_id");
+//数据回显
+$.get("/v6/course/basic", { "cs_id": cs_id }, function(data) {
+    if (data.code == 200) {
+        $("#course-edit1").append(template("course-edit1-tpl", data.result))
+            // location.href = "/dist/html/course/course_edit_step2.html?cs_id=" + data.result.cs_id;
+    }
+})
