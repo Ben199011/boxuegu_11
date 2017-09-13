783fb75e512f95fe0d4df06d27c1c60b086f2eda
Ben <510662506.@qq.com>
2017-09-13 20:56:20 +0800

    编辑模态框的数据回显

--- src/js/course/edit3.js
+++ src/js/course/edit3.js
@@ -4,8 +4,17 @@ var util = require('../common/util.js');
 var cs_id = util.getSearch('cs_id');
 //数据回显
 $.get("/v6/course/lesson", { cs_id: cs_id }, function(data) {
-    if (data.code == 200) {
-        data.result.editIndex = 3;
-        $("#course-edit3").append(template("course-edit3-tpl", data.result))
-    }
+        if (data.code == 200) {
+            data.result.editIndex = 3;
+            $("#course-edit3").append(template("course-edit3-tpl", data.result))
+        }
+    })
+    //模态框的数据回显
+$(document).on("click", ".btn-lesson-edit", function() {
+    var ct_id = $(this).attr("data-id");
+    $.get("/v6/course/chapter/edit", { ct_id: ct_id }, function(data) {
+        if (data.code == 200) {
+            $("#chapterModal").html(template("lesssons-tpl", data.result))
+        }
+    })
 })
