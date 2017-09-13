e40e7e9980692ef767b67e8238c49ed080219319
Ben <510662506.@qq.com>
2017-09-13 14:45:42 +0800

    二级联动和表单提交

--- src/js/course/edit1.js
+++ src/js/course/edit1.js
@@ -4,8 +4,32 @@ var util = require('../common/util')
 var cs_id = util.getSearch("cs_id");
 //数据回显
 $.get("/v6/course/basic", { "cs_id": cs_id }, function(data) {
-    if (data.code == 200) {
-        $("#course-edit1").append(template("course-edit1-tpl", data.result))
-            // location.href = "/dist/html/course/course_edit_step2.html?cs_id=" + data.result.cs_id;
+        if (data.code == 200) {
+            $("#course-edit1").append(template("course-edit1-tpl", data.result))
+        }
+    })
+    //二级联动
+    // 因为整个数据回显是动态构建的，所以通过委托的方式监听父级学科select的change事件
+$(document).on("change", "#sel-cgTop", function() {
+        var topCgid = $(this).val();
+        $.get("/v6/category/child", { cg_id: topCgid }, function(data) {
+            var html = '';
+            if (data.code == 200) {
+                for (var i = 0; i < data.result.length; i++) {
+                    html += '<option value="' + data.result[i].cg_id + '">' + data.result[i].cg_name + '</option>'
+                }
+                $("#sel-cgChild").html(html);
+            }
+        })
+    })
+    //数据提交
+$("#course-edit1-form").ajaxForm({
+    delegation: true,
+    success: function(data) {
+        if (data.code == 200) {
+            alert("保存成功");
+            location.href = '/dist/html/course/course_edit_step2.html?cs_id=' + cs_id;
+        }
     }
+
 })
