ee188b366ef73c7f450f1cbc34485c396e4dc44e
Ben <510662506.@qq.com>
2017-09-13 20:18:18 +0800

    课程编辑第三步数据回显

--- src/js/category/edit.js
+++ src/js/category/edit.js
@@ -4,7 +4,6 @@ var util = require("../common/util");
 var cg_id = util.getSearch("cg_id");
 //回显
 $.get("/v6/category/edit", { "cg_id": cg_id }, function(data) {
-        console.log(template("category-edit-tpl", data.result))
         $(".category-edit").html(template("category-edit-tpl", data.result))
     })
     //  表单提交：
--- src/js/course/edit3.js
+++ src/js/course/edit3.js
@@ -1,2 +1,11 @@
 require("../common/header")
 require("../common/aside")
+var util = require('../common/util.js');
+var cs_id = util.getSearch('cs_id');
+//数据回显
+$.get("/v6/course/lesson", { cs_id: cs_id }, function(data) {
+    if (data.code == 200) {
+        data.result.editIndex = 3;
+        $("#course-edit3").append(template("course-edit3-tpl", data.result))
+    }
+})
