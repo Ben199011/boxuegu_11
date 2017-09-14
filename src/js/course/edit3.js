e7c9bcdf0aa51ac531886822b0e320b7fb22791d
Ben <510662506.@qq.com>
2017-09-14 08:45:47 +0800

    模态框的编辑和添加

--- src/js/course/edit3.js
+++ src/js/course/edit3.js
@@ -11,10 +11,30 @@ $.get("/v6/course/lesson", { cs_id: cs_id }, function(data) {
     })
     //模态框的数据回显
 $(document).on("click", ".btn-lesson-edit", function() {
-    var ct_id = $(this).attr("data-id");
-    $.get("/v6/course/chapter/edit", { ct_id: ct_id }, function(data) {
-        if (data.code == 200) {
-            $("#chapterModal").html(template("lesssons-tpl", data.result))
-        }
+        var ct_id = $(this).attr("data-id");
+        $.get("/v6/course/chapter/edit", { ct_id: ct_id }, function(data) {
+            if (data.code == 200) {
+                $("#chapterModal").html(template("lesssons-tpl", data.result))
+            }
+        })
     })
+    //编辑和添加课时同用一个模态框，所以一个请求就可以了
+    //编辑和添加数据的提交
+$("#lesson-form").ajaxForm({
+    delegation: true,
+    success: function(data) {
+        if (data.code == 200) {
+            if (data.result) {
+                alert("添加成功")
+                $("#lesson-form")[0].reset();
+                window.location.reload()
+            } else {
+                alert("编辑成功")
+                window.location.reload()
+            }
+        }
+    }
+})
+$(document).on("click", ".btn-add-edit3", function() {
+    $("#chapterModal").html(template("lesssons-tpl", { cs_id: cs_id }))
 })
